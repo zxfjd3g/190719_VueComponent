8072b86e540ddb8ab032a0d339d6a99286d40974
zxfjd3g <258147149@qq.com>
2019-12-09 08:20:03 +0800

    event-bus

--- custom/event-bus.js
+++ custom/event-bus.js
@@ -5,5 +5,31 @@
 (function (window) {
   const eventBus = {}
 
+  let listenerContainer = {}
+
+  eventBus.on = function (eventName, listener) {
+    const listeners = listenerContainer[eventName]
+    if (listeners) {
+      listeners.push(listener)
+    } else {
+      listenerContainer[eventName] = [listener]
+    }
+  }
+
+  eventBus.emit = function (eventName, data) {
+    const listeners = listenerContainer[eventName]
+    if (listeners && listeners.length>0) {
+      listeners.forEach(listener => listener(data))
+    }
+  }
+
+  eventBus.off = function (eventName) {
+    if (eventName===undefined) {
+      listenerContainer = {}
+    } else {
+      delete listenerContainer[eventName]
+    }
+  }
+
   window.eventBus = eventBus
 })(window)
